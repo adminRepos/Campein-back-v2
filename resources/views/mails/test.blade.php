56a8f42f81f9874fb8d23c6ed2785b7471066de7
Holman Calderón <desarrollador2@intevo.com.co>
2023-05-19 14:30:58 -0500

    correcion de puerto de url de correos

--- resources/views/mails/test.blade.php
+++ resources/views/mails/test.blade.php
@@ -1,5 +1,5 @@
 <h1>Buen día {{$nombre}}</h1>
 <p>Da click en el siguiente enlace para continuar con tu proceso de restablecimiento de contraseña de CAMPEIN.</p>
 <p>Este link tiene una validez de 24 horas o un solo uso.</p>
-<a href="http://190.131.244.220:10259/#/auth/restablecerPass?opc={{$id}}&opc2={{$date}}">Click aqui</a>
+<a href="http://190.131.244.220:10258/#/auth/restablecerPass?opc={{$id}}&opc2={{$date}}">Click aqui</a>
 {{-- <a href="http://localhost:4200/#/auth/restablecerPass?opc={{$id}}&opc2={{$date}}">Click aqui</a> --}}
