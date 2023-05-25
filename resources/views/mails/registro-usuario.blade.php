3f9aeb22d7feb5940263230a043bc4683f5c9224
Holman Calderón <desarrollador2@intevo.com.co>
2023-05-25 13:10:19 -0500

    se ajusta rutas de envio en correos para prod

--- resources/views/mails/registro-usuario.blade.php
+++ resources/views/mails/registro-usuario.blade.php
@@ -4,4 +4,4 @@
 <h3>Contraseña: {{$pass}}</h3>
 
 <p><b>No compartas estás credenciales a ninguna persona.</b></p>
-<a href="http://190.131.244.220:10259/#/auth/login">Click aqui para ir al portal</a>
+<a href="https://campein.com/#/auth/login">Click aqui para ir al portal</a>
